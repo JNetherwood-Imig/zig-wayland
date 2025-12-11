1a48f3a6d608cda261c9cd4657a402bc54e966fc
Jackson Netherwood-Imig <jacksonni752@gmail.com>
2025-12-11 10:14:29 -0800

    more scanner v2 progress

--- src/scanner.zig
+++ src/scanner.zig
@@ -1,13 +1,17 @@
 const std = @import("std");
 const xml = @import("xml");
 const log = std.log.scoped(.scanner);
+const Allocator = std.mem.Allocator;
 
 pub fn main() !void {
     var gpa_state = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa_state.deinit();
     const gpa = gpa_state.allocator();
 
-    var mode = Mode.client;
+    var context = try Context.init(gpa);
+    defer context.deinit(gpa);
+
+    var mode = Mode.client; // Default to generating client code, since that's most likely
     var output_file: ?[]const u8 = null;
     var current_prefix: []const u8 = "";
 
@@ -33,26 +37,107 @@ pub fn main() !void {
             const file = try std.fs.cwd().openFile(path, .{});
             defer file.close();
 
+            // Set up Io.Reader for input file
             var input_buffer: [4096]u8 = undefined;
             var input_reader = file.reader(&input_buffer);
             const io_reader = &input_reader.interface;
 
+            // Get xml.Reader for document parsing
             var stream_reader = xml.Reader.Streaming.init(gpa, io_reader, .{});
             defer stream_reader.deinit();
             const xml_reader = &stream_reader.interface;
 
-            try parseDoc(xml_reader);
+            // Parse document and append to context
+            try context.parseDocument(gpa, xml_reader, current_prefix);
         }
     }
 }
 
 const Context = struct {
-    protocols: std.ArrayList(),
+    protocols: std.ArrayList(Protocol),
+
+    pub fn init(gpa: Allocator) !Context {
+        return .{
+            .protocols = try .initCapacity(gpa, 4),
+        };
+    }
+
+    pub fn deinit(self: *Context, gpa: Allocator) void {
+        for (self.protocols.items) |*protocol|
+            protocol.deinit(gpa);
+        self.protocols.deinit(gpa);
+    }
+
+    fn parseDocument(self: *Context, gpa: Allocator, reader: *xml.Reader, prefix: []const u8) !void {
+        while (reader.read()) |node| switch (node) {
+            .eof => return error.UnexpectedEof,
+            .element_start => {
+                const elem = reader.elementName();
+                if (!std.mem.eql(u8, elem, "protocol")) continue;
+                var protocol = try Protocol.parse(gpa, reader, prefix);
+                errdefer protocol.deinit(gpa);
+                try self.protocols.append(gpa, protocol);
+            },
+            else => continue,
+        } else |err| return err;
+    }
 
     const Protocol = struct {
+        prefix: []const u8,
         name: []const u8,
         description: ?Description = null,
         interfaces: std.ArrayList(Interface),
+
+        pub fn parse(gpa: Allocator, reader: *xml.Reader, prefix: []const u8) !Protocol {
+            const name = for (0..reader.attributeCount()) |i| {
+                const attrib = reader.attributeName(i);
+                if (!std.mem.eql(u8, attrib, "name")) continue;
+                break try reader.attributeValueAlloc(gpa, i);
+            } else return error.NameNotFound;
+            errdefer gpa.free(name);
+
+            var description: ?Description = null;
+            errdefer if (description) |*desc| desc.deinit(gpa);
+
+            var interfaces = try std.ArrayList(Interface).initCapacity(gpa, 4);
+            errdefer interfaces.deinit(gpa);
+
+            while (reader.read()) |node| switch (node) {
+                .eof => return error.UnexpectedEof,
+                .element_start => {
+                    const elem = reader.elementName();
+                    if (std.mem.eql(u8, elem, "description"))
+                        description = try Description.parse(reader, gpa)
+                    else if (std.mem.eql(u8, elem, "copyright"))
+                        continue
+                    else if (std.mem.eql(u8, elem, "interface")) {
+                        var interface = try Interface.parse(gpa);
+                        errdefer interface.deinit(gpa);
+                        try interfaces.append(gpa, interface);
+                    } else {
+                        log.err("Unexpected protocol element {s}.", .{elem});
+                        return error.UnexpectedElement;
+                    }
+                },
+                else => continue,
+            } else |err| return err;
+
+            return .{
+                .prefix = try gpa.dupe(u8, prefix),
+                .name = name,
+                .description = description,
+                .interfaces = interfaces,
+            };
+        }
+
+        pub fn deinit(self: *Protocol, gpa: Allocator) void {
+            gpa.free(self.prefix);
+            gpa.free(self.name);
+            if (self.description) |*description| description.deinit(gpa);
+            for (self.interfaces.items) |*interface|
+                interface.deinit(gpa);
+            self.interfaces.deinit(gpa);
+        }
     };
 
     const Interface = struct {
@@ -62,6 +147,68 @@ const Context = struct {
         requests: std.ArrayList(Request),
         events: std.ArrayList(Event),
         enums: std.ArrayList(Enum),
+
+        pub fn parse(gpa: Allocator, reader: *xml.Reader) !Interface {
+            var name: ?[]const u8 = null;
+            errdefer if (name) |n| gpa.free(n);
+            var version: ?u32 = null;
+
+            for (0..reader.attributeCount()) |i| {
+                const attrib = reader.attributeName(i);
+                if (std.mem.eql(u8, attrib, "name"))
+                    name = try reader.attributeValueAlloc(gpa, i)
+                else if (std.mem.eql(u8, attrib, "version"))
+                    version = try std.fmt.parseInt(u32, reader.attributeValueRaw(i), 10)
+                else
+                    continue;
+            }
+
+            var description: ?Description = null;
+            errdefer if (description) |*desc| desc.deinit(gpa);
+
+            var requests = try std.ArrayList(Request).initCapacity(gpa, 8);
+            errdefer {
+                for (requests.items) |*req| req.deinit(gpa);
+                requests.deinit(gpa);
+            }
+
+            var events = try std.ArrayList(Event).initCapacity(gpa, 8);
+            errdefer {
+                for (events.items) |*ev| ev.deinit(gpa);
+                events.deinit(gpa);
+            }
+
+            var enums = try std.ArrayList(Enum).initCapacity(gpa, 4);
+            errdefer {
+                for (enums.items) |*en| en.deinit(gpa);
+                enums.deinit(gpa);
+            }
+
+            while (reader.read()) |node| switch (node) {
+                .eof => return error.UnexpectedEof,
+                else => continue,
+            } else |err| return err;
+
+            return .{
+                .name = name orelse return error.NameNotFound,
+                .version = version orelse return error.VersionNotFound,
+                .description = description,
+                .requests = requests,
+                .events = events,
+                .enums = enums,
+            };
+        }
+
+        pub fn deinit(self: *Interface, gpa: Allocator) void {
+            gpa.free(self.name);
+            if (self.description) |*desc| desc.deinit(gpa);
+            for (self.requests.items) |*req| req.deinit(gpa);
+            for (self.events.items) |*ev| ev.deinit(gpa);
+            for (self.enums.items) |*en| en.deinit(gpa);
+            self.requests.deinit(gpa);
+            self.events.deinit(gpa);
+            self.enums.deinit(gpa);
+        }
     };
 
     const Request = struct {
@@ -71,6 +218,11 @@ const Context = struct {
         deprecated_since: u32 = 0,
         description: ?Description = null,
         args: std.ArrayList(Arg),
+
+        pub fn deinit(self: *Request, gpa: Allocator) void {
+            _ = self;
+            _ = gpa;
+        }
     };
 
     const Event = struct {
@@ -80,6 +232,11 @@ const Context = struct {
         deprecated_since: u32 = 0,
         description: ?Description = null,
         args: std.ArrayList(Arg),
+
+        pub fn deinit(self: *Event, gpa: Allocator) void {
+            _ = self;
+            _ = gpa;
+        }
     };
 
     const Enum = struct {
@@ -88,6 +245,11 @@ const Context = struct {
         type: enum { none, bitfield } = .none,
         description: ?Description = null,
         entries: std.ArrayList(Entry),
+
+        pub fn deinit(self: *Enum, gpa: Allocator) void {
+            _ = self;
+            _ = gpa;
+        }
     };
 
     const Entry = struct {
@@ -101,7 +263,9 @@ const Context = struct {
 
     const Arg = struct {
         name: []const u8,
-        type: enum { int, uint, fixed, object, new_id, string, array, fd },
+        type: Type,
+        summary: ?[]const u8 = null,
+
         const Type = union(enum) {
             int: void,
             uint: void,
@@ -118,43 +282,39 @@ const Context = struct {
 
     const Description = struct {
         summary: []const u8,
-        body: []const u8,
+        body: ?[]const u8 = null,
+
+        pub fn parse(reader: *xml.Reader, gpa: Allocator) !Description {
+            const summary = for (0..reader.attributeCount()) |i| {
+                const attrib = reader.attributeName(i);
+                if (!std.mem.eql(u8, attrib, "summary")) continue;
+                break try reader.attributeValueAlloc(gpa, i);
+            } else return error.SummaryNotFound;
+            errdefer gpa.free(summary);
+
+            const body: ?[]const u8 = null;
+            while (reader.read()) |node| switch (node) {
+                .eof => return error.UnexpectedEof,
+                .element_end => {
+                    const name = reader.elementName();
+                    if (!std.mem.eql(u8, name, "description")) return error.UnexpectedElementEnd;
+                    break;
+                },
+                .text, .cdata => {
+                    std.debug.print("Description body is {t}\n", .{node});
+                },
+                else => continue,
+            } else |err| return err;
+
+            return .{ .summary = summary, .body = body };
+        }
+
+        pub fn deinit(self: *Description, gpa: Allocator) void {
+            gpa.free(self.summary);
+            if (self.body) |body| gpa.free(body);
+        }
     };
 };
-
-fn parseDoc(reader: *xml.Reader) !void {
-    while (reader.read()) |node| switch (node) {
-        .eof => return error.UnexpectedEof,
-        .element_start => {
-            const name = reader.elementName();
-            try parseProtocol(reader, name);
-        },
-        .element_end => {},
-        inline else => |n| log.debug("Got {t}.", .{n}),
-    } else |err| switch (err) {
-        error.MalformedXml => {
-            const loc = reader.errorLocation();
-            const code = reader.errorCode();
-            log.err("{f}: {t}", .{ loc, code });
-            return error.MalformedXml;
-        },
-        else => |e| return e,
-    }
-}
-
-fn parseProtocol(reader: *xml.Reader, name: []const u8) !void {
-    while (reader.read()) |node| switch (node) {
-        .eof => return error.UnexpectedEof,
-        .element_end => {
-            const end_name = reader.elementName();
-            if (std.mem.eql(u8, end_name, name)) {
-                // TODO
-                return;
-            } else return error.UnexpectedElementEnd;
-        },
-        else => continue,
-    } else |err| return err;
-}
 
 const Mode = enum {
     client,
