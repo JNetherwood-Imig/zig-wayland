bccdf7d01ec271afd8ec69fdec1611d72d864dd1
Jackson Netherwood-Imig <jacksonni752@gmail.com>
2025-12-23 12:41:12 -0800

    chore(examples/hello-world): improve comments and code style

--- examples/hello-world/src/main.zig
+++ examples/hello-world/src/main.zig
@@ -27,7 +27,10 @@ pub fn main() !void {
     var ida_state = wayland.IdAllocator.Bounded.init(&id_buf, .client);
     const ida = ida_state.id_allocator();
 
+    // Using `.auto` here defers socket resolution until connect time.
     var sock_info: wayland.SocketInfo = .auto;
+    // Connect to the socket, storing the resolved socket info in `sock_info` so that it can be
+    // used in the error message if connecting fails.
     conn = sock_info.connect(ida) catch |err| {
         std.log.err("Failed to connect to {f}.", .{sock_info});
         return err;
@@ -53,13 +56,10 @@ pub fn main() !void {
     const sync_cb = try disp.sync(&conn);
     try handler.addObjectBounded(sync_cb);
 
+    // Wait for all registry global events here to discover globals.
+    // After the last global is sent, the server will send a `wl_callback.done` event
+    // for `sync_cb`.
     while (handler.waitNextMessage(&conn)) |event| switch (event) {
-        .wl_callback => |cb| {
-            std.debug.assert(cb.done.wl_callback == sync_cb);
-            const data = cb.done.callback_data;
-            std.log.debug("Sync callback data: {d}.", .{data});
-            break;
-        },
         .wl_registry => |ev| switch (ev) {
             .global => |g| {
                 // Bind to globals
@@ -79,6 +79,8 @@ pub fn main() !void {
             },
             .global_remove => {},
         },
+        // All globals have been received, we can continue.
+        .wl_callback => break,
         else => std.log.err("Unexpected event: {}.", .{event}),
     } else |e| return e;
 
@@ -93,6 +95,7 @@ pub fn main() !void {
     const toplevel = try xdg_surf.getToplevel(&conn);
     try handler.addObjectBounded(toplevel);
 
+    // Perform initial surface commit to begin surface lifecycle.
     try surf.commit(&conn);
 
     // Main loop
@@ -101,30 +104,41 @@ pub fn main() !void {
         .xdg_surface => |ev| {
             try xdg_surf.ackConfigure(&conn, ev.configure.serial);
             if (!configured) {
+                // Create and register the buffer.
                 const buffer = try createBuffer();
                 try handler.addObjectBounded(buffer);
+                // Attach buffer to our surface so it can be presented.
                 try surf.attach(&conn, buffer, 0, 0);
             }
             try surf.commit(&conn);
             configured = true;
         },
+        // The only xdg toplevel event we care about is `close`.
         .xdg_toplevel => |ev| switch (ev) {
             .close => break,
             else => {},
         },
         .wl_display => |ev| switch (ev) {
+            // There is no internal handling of the `wl_display.delete_id` event, so an application
+            // **must** handle the event by returning the id to the allocator and removing the
+            // object from the event handler.
             .delete_id => |id| {
                 handler.delObject(id.id);
                 try ida.free(id.id);
             },
+            // Much more advanced handling of the error event could easily be done,
+            // but it is unnecessary for the scope of this example.
             .@"error" => return error.ProtocolError,
         },
         else => {},
     } else |e| return e;
 
-    if (shm_data.len > 0) std.posix.munmap(shm_data);
+    // Begin cleanup by unmapping the shm buffer data.
+    // The rest of cleanup will happen with defers.
+    std.posix.munmap(shm_data);
 }
 
+// Create a wl_buffer backed by shm.
 fn createBuffer() !wl.Buffer {
     const stride = width * 4;
     const size = stride * height;
@@ -140,53 +154,60 @@ fn createBuffer() !wl.Buffer {
         fd,
         0,
     );
+
+    // Fill buffer with white pixels.
     @memset(shm_data, 255);
 
     // Shm pools have no events, so we don't register it.
     const pool = try shm.createPool(&conn, fd, size);
     defer pool.destroy(&conn) catch {};
 
-    // We do need to register the buffer, but we'll do that when we return it.
+    // We do need to register the buffer, but we'll do that in the parent scope once we return it.
     const buffer = try pool.createBuffer(&conn, 0, width, height, stride, .argb8888);
     return buffer;
 }
 
-// The following utility functions are quick and dirty replacements
-// for the ones from wayland-book.com, written without shm* functions
-// because those are provided by libc.
+// The following utility functions are quick and dirty replacements for the ones
+// from wayland-book.com, written without shm_* functions because those are provided
+// by libc and not available in zig std.os.linux or std.posix.
 
+/// Allocate an shm file descriptor, truncated to `size` bytes.
 fn allocateShmFile(size: usize) !std.posix.fd_t {
     const fd = try createShmFile();
     try std.posix.ftruncate(fd, size);
     return fd;
 }
 
+/// Create an shm file descriptor.
 fn createShmFile() !std.posix.fd_t {
-    while (true) {
-        var path: [22:0]u8 = @splat(0);
-        @memcpy(path[0..16], "/dev/shm/wl_shm-");
-        try randomize(path[path.len - 6 ..]);
-        const fd = std.posix.open(
-            &path,
-            .{
-                .ACCMODE = .RDWR,
-                .CREAT = true,
-                .CLOEXEC = true,
-                .EXCL = true,
-                .NOFOLLOW = true,
-            },
-            0o0600,
-        ) catch continue;
-        try std.posix.unlink(&path);
-        return fd;
-    }
+    const shm_prefix = "/dev/shm/wl_shm-";
+    const shm_perms = 0o0600;
+    const shm_opts: std.posix.O = .{
+        .ACCMODE = .RDWR,
+        .CREAT = true,
+        .CLOEXEC = true,
+        .EXCL = true,
+        .NOFOLLOW = true,
+    };
+
+    var path: [22:0]u8 = @splat(0);
+    @memcpy(path[0..shm_prefix.len], shm_prefix);
+
+    const fd = while (true) {
+        try randomize(path[shm_prefix.len..]);
+        break std.posix.open(&path, shm_opts, shm_perms) catch continue;
+    };
+
+    try std.posix.unlink(&path);
+    return fd;
 }
 
+/// Fill `buf` with random characters.
 fn randomize(buf: []u8) !void {
     const ts = try std.posix.clock_gettime(.REALTIME);
     var r = ts.nsec;
-    for (0..buf.len) |i| {
-        buf[i] = 'A' + @as(u8, @intCast((r & 15) + (r & 16) * 2));
+    for (buf) |*byte| {
+        byte.* = 'A' + @as(u8, @intCast((r & 15) + (r & 16) * 2));
         r >>= 5;
     }
 }
